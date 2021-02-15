d76173c7ff5db7de52b011df6d4b9cd4527b8ba7
Noah <noahschnur@gmail.com>
2021-02-15 10:31:43 -0800

    Prompt length

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var txt;
+  var generate = prompt("How long do you need your new password?")
+    if (generate <=8){
+      window.prompt("Would you like to include lowercase letters?")
+    } else {
+      window.alert("Please enter a number between 8 and 128")
+    }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
