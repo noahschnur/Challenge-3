eb567831f4b95647839951bb75cd34f2ac7d5a11
Noah <noahschnur@gmail.com>
2021-02-21 13:24:20 -0800

    add window confirm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
-// Assignment code here
+;
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var txt;
-  var generate = prompt("How long do you need your new password?")
-    if (generate <=8){
-      window.prompt("Would you like to include lowercase letters?")
-    } else {
-      window.alert("Please enter a number between 8 and 128")
-    }
+    var passwordLength = window.prompt("How long do you want you password? Pick any length between 8 and 128 characters.");
+      if(passwordLength < 129 || passwordLength > 8) {
+        console.log(passwordLength);
+      } else {
+        window.alert("Please pick a number between 8 and 128");
+      }
+
+    window.confirm("Would you like to use lowercase letters in your password?");
+    window.confirm("Would you like to use uppercase letters in your password?");
+    window.confirm("Would you like to use numbers in your password?");
+    window.confirm("Would you like to use special characters in your password?");
+    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
