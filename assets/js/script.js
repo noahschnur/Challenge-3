a789f2af039d55b39573f3be0039ec508c87a876
Noah <noahschnur@gmail.com>
2021-02-21 17:48:58 -0800

    added notations, rewrite prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,36 @@
-;
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var passwordLength = window.prompt("How long do you want you password? Pick any length between 8 and 128 characters.");
-      if(passwordLength < 129 || passwordLength > 8) {
-        console.log(passwordLength);
-      } else {
-        window.alert("Please pick a number between 8 and 128");
-      }
+  //prompt for length
+  var passwordLength = parseInt(prompt("How long do you want you password? Pick any length between 8 and 128 characters."));
+    if (length < 8) {
+      alert("Remember, the password must be between 8 and 128 characters.");
+      return;
+    }
+    if (lenth > 128) {
+      alert("Remember, the password must be between 8 and 128 characters.");
+      return;
+    }
+    //promt for lowercase letters
+    var lower = window.confirm("Would you like to use lowercase letters in your password?");
+    //promt for uppercase letters
+    var upper = window.confirm("Would you like to use uppercase letters in your password?");
+    //promt for numbers
+    var numbers = window.confirm("Would you like to use numbers in your password?");
+    //promt for special characters
+    var special = window.confirm("Would you like to use special characters in your password?");
 
-    window.confirm("Would you like to use lowercase letters in your password?");
-    window.confirm("Would you like to use uppercase letters in your password?");
-    window.confirm("Would you like to use numbers in your password?");
-    window.confirm("Would you like to use special characters in your password?");
-    
-  var password = generatePassword();
+    while (!lower && !upper && !numbers && !special) {
+      alert("Please select at least one character type!");
+      lower = window.confirm("Would you like to use lowercase letters in your password?");
+      upper = window.confirm("Would you like to use uppercase letters in your password?");
+      numbers = window.confirm("Would you like to use numbers in your password?");
+      special = window.confirm("Would you like to use special characters in your password?");
+    }
+
+
+    var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
