94e94452280a7553376566df75ec9fb5d5b23dfb
Noah <noahschnur@gmail.com>
2021-02-21 23:23:19 -0800

    final draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var generateBtn = document.querySelector("#generate");
-//definitions for characters
+//arrays
 var characterArray = [];
 var lowerCh = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 var upperCh = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
@@ -46,7 +46,7 @@ function writePassword() {
       for (let i = 0; i < length; i++) {
         var randomPassword = randomPassword + characterArray(Math.floor(Math.random() * characterArray.length));
       
-      console.log(randomPassword);
+      return randomPassword;
       }
       
       document.getElementById("password").textContent = randomPassword
